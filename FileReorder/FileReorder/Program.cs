b3f526dd61e1057901cf7777eebe4268f54a9b7f
Euglenach <midorimushiiii@gmail.com>
2023-10-06 04:20:17 +0900

    だいたいの流れ

--- FileReorder/FileReorder/Program.cs
+++ FileReorder/FileReorder/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FileReorder;
+using System.Collections.Immutable;
 
-Console.WriteLine("Hello, World!");
+IContentSource<FileInfo> source = new FileTxtListSource();
+IContentProcessor<FileInfo> processor = new FileProcessor(FileProcessor.OrderKind.CreatedAtAndUpdatedAt);
+
+Reorder(source, processor);
+
+static void Reorder<T>(IContentSource<T> source, IContentProcessor<T> processor)
+{
+    var contents = source.GetContents().ToImmutableArray();
+    
+    for(var i = 0; i < contents.Length; i++)
+    {
+        var content = contents[i];
+        processor.Processing(content, i);
+    }
+}
